0dd47ab9a858f13caeae7ca3912b9ca8a06211dd
slainer68 <slainer68@gmail.com>
2012-02-16 03:52:51 +0100

    no more timecop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'paybox_system'
-require 'timecop'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
